c9875424b1ba4221c3879b28722cee4391dba7be
Erik Erlandson <eerlands@redhat.com>
2010-12-15 22:52:50 -0700

    Factored testing macros out to ut_common.h

--- test/ut_raw.cpp
+++ test/ut_raw.cpp
@@ -1,48 +1,10 @@
 #include <boost/test/unit_test.hpp>
 
-#include <iostream>
-#include <sstream>
-
 #include "ootree.h"
-
-using namespace ootree;
-using namespace std;
+#include "ut_common.h"
 
 
 BOOST_AUTO_TEST_SUITE(ut_raw)
-
-
-#define CHECK_TREE(t, meth, ref) { \
-    stringstream tst; \
-\
-    for (typeof(t.bf_end()) j(t.bf_begin());  j != t.bf_end();  ++j) { \
-        if (j != t.bf_begin()) tst << " "; \
-        tst << j->meth; \
-    } \
-    BOOST_CHECK_MESSAGE(tst.str() == ref, "Checking " << #meth << ": Expected \"" << ref << "\" received \"" << tst.str() << "\"\n"); \
-}
-
-
-#define CHECK_TREE_DF_POST(t, meth, ref) { \
-    stringstream tst; \
-\
-    for (typeof(t.df_post_end()) j(t.df_post_begin());  j != t.df_post_end();  ++j) { \
-        if (j != t.df_post_begin()) tst << " "; \
-        tst << j->meth; \
-    } \
-    BOOST_CHECK_MESSAGE(tst.str() == ref, "Checking " << #meth << ": Expected \"" << ref << "\" received \"" << tst.str() << "\"\n"); \
-}
-
-
-#define CHECK_TREE_DF_PRE(t, meth, ref) { \
-    stringstream tst; \
-\
-    for (typeof(t.df_pre_end()) j(t.df_pre_begin());  j != t.df_pre_end();  ++j) { \
-        if (j != t.df_pre_begin()) tst << " "; \
-        tst << j->meth; \
-    } \
-    BOOST_CHECK_MESSAGE(tst.str() == ref, "Checking " << #meth << ": Expected \"" << ref << "\" received \"" << tst.str() << "\"\n"); \
-}
 
 
 BOOST_AUTO_TEST_CASE(default_ctor) {
