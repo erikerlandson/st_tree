843ff0949d7e455f1b2ba44c8d30b5d7c0fc5ff9
Erik Erlandson <eerlands@redhat.com>
2011-05-02 21:39:23 -0700

    added correct header and guard for ut_common.h

--- test/ut_common.h
+++ test/ut_common.h
@@ -1,3 +1,28 @@
+/******
+st_tree: An easy to use and highly configurable C++ template tree class, 
+using STL container style interfaces.
+
+Copyright (c) 2010-2011 Erik Erlandson
+
+Author:  Erik Erlandson <erikerlandson@yahoo.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+******/
+
+#if !defined(__ut_common_h__)
+#define __ut_common_h__ 1
+
+
 #include <iostream>
 #include <sstream>
 
@@ -80,3 +105,5 @@ typename Tree::const_df_post_iterator& df_post_j(const Tree& t) {
     BOOST_CHECK_MESSAGE(tst.str() == ref, "Checking " << #meth << ": Expected \"" << ref << "\" received \"" << tst.str() << "\"\n"); \
 }
 
+
+#endif
